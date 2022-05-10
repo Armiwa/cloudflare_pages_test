94d2e8ae64eccce6595294cf2a35738f51833b70
Armiwa <armiwa@packetplay.com>
2022-05-10 20:08:58 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>{process.env.NODE_ENV}</h1>
+          <h1>{process.env.REACT_APP_SERVICE_COM}</h1>
         </p>
         <a
           className="App-link"
